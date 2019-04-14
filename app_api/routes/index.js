b6d1eb74e656a3dd819c58822e2fe5ed45843c73
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 15:12:13 -0400

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,12 +1,19 @@
-
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({
+	secret: process.env.JWT_SECRET,
+	userProperty: 'payload'
+});
 var ctrlBlog = require('../controllers/blogs');
+var ctrlAuth = require('../controllers/authentication');
 
 router.get('/blogs', ctrlBlog.blogList);
 router.post('/blogs', ctrlBlog.blogCreate);
 router.get('/blogs/:blogid', ctrlBlog.blogReadOne);
 router.put('/blogs/:blogid', ctrlBlog.blogUpdateOne);
 router.delete('/blogs/:blogid', ctrlBlog.blogDeleteOne);
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;
