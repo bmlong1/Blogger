c0e8f9bb8c561ed262f665cd759359bd7e8f8abc
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 15:31:46 -0400

    Update bloggerApp.js

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -165,18 +165,3 @@ app.controller('DeleteController', ['$http', '$routeParams', '$location', functi
 		});
 	}
 }]);
-
-app.controller('NavigationController', ['$state', '$location', 'authentication', function NavigationController($state, $location, authentication) {
-    var vm = this;
-    vm.currentPath = $location.path();
-    vm.currentUser = function()  {
-        return authentication.currentUser();
-    }
-    vm.isLoggedIn = function() {
-        return authentication.isLoggedIn();
-    }
-    vm.logout = function() {
-      authentication.logout();
-      $location.path('/');
-    };
-}]);
