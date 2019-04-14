fb9df6ec12a30c883b94309bd30ad4aea66d5dc5
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 16:57:10 -0400

    Update bloggerApp.js

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -1,18 +1,4 @@
 var app = angular.module('bloggerApp', ['ngRoute']);
-app.controller('NavigationController', ['$state', '$location', 'authentication', function NavigationController($state, $location, authentication) {
-    var vm = this;
-    vm.currentPath = $location.path();
-    vm.currentUser = function()  {
-        return authentication.currentUser();
-    }
-    vm.isLoggedIn = function() {
-        return authentication.isLoggedIn();
-    }
-    vm.logout = function() {
-      authentication.logout();
-      $location.path('/');
-    };
-}]);
 
 //*** Router Provider ***//
 app.config( function($routeProvider) {
