043018ad67aa953eaffe23fa8dd7826b50c28f51
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 18:59:11 -0400

    Update bloggerApp.js

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -159,11 +159,16 @@ app.controller('AddController', ['$http', '$location', 'authentication',  functi
 	};
 }]);
 
-app.controller('ListController', function ListController($http) {
+app.controller('ListController', function ListController($http, authentication) {
     var vm = this;
     vm.pageHeader = {
         title: 'Blog List'
     };
+	
+	 vm.isLoggedIn = function() {
+        return authentication.isLoggedIn();
+    }
+	
     getAllBlogs($http).success(function(data) {
 		vm.blogs = data;
         vm.message = "Blog data found!";
