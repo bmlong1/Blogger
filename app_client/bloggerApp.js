d2efcf5e622936d2f86fff6da3bc13cc621196e2
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 19:32:28 -0400

    Update bloggerApp.js

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -66,46 +66,44 @@ app.service('authentication', authentication);
 }
 //*** Router Provider ***//
 app.config( function($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
-	$locationProvider.hashPrefix('');
 	$routeProvider
-		.when('/', {
+		.when('#!/', {
 			templateUrl: 'pages/home.html',
 			controller: 'HomeController',
 			controllerAs: 'vm'
 		})
 
-		.when('/blog-list', {
+		.when('#!/blog-list', {
 			templateUrl: 'pages/blog-list.html',
 			controller : 'ListController',
 			controllerAs: 'vm'
 		})
 
-		.when('/blog-add', {
+		.when('#!/blog-add', {
 			templateUrl: 'pages/blog-add.html',
 			controller: 'AddController',
 			controllerAs: 'vm'
 		})
           
-        .when('/blog-edit/:id', {
+        .when('#!/blog-edit/:id', {
 			templateUrl: 'pages/blog-edit.html',
 			controller: 'EditController',
 			controllerAs: 'vm'
 		})
         
-        .when('/blog-delete/:id', {
+        .when('#!/blog-delete/:id', {
 			templateUrl: 'pages/blog-delete.html',
             controller: 'DeleteController',
             controllerAs: 'vm'
         })
 	
-		.when('/register', {
+		.when('#!/register', {
 			templateUrl: 'pages/register.html',
 			controller: 'RegisterController',
 			controllerAs: 'vm'
 		})
 		
-		.when('/login', {
+		.when('#!/login', {
 			templateUrl: 'pages/login.html',
 			controller: 'LoginController',
 			controllerAs: 'vm'
