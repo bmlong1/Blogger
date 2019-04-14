4c23b0ef6ce224d8dd497fb1c9d3da9263af005d
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 14:33:46 -0400

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -20,31 +20,32 @@ mongoose.connection.on('disconnected', function() {
 
 // Closes (disconnects) from Mongoose DB upon shutdown                       
 gracefulShutdown = function (msg, callback) {
-  mongoose.connection.close(function () {
-    console.log('Mongoose disconnected through ' + msg);
-    callback();
-  });
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
 };
 
 // For nodemon restarts
 process.once('SIGUSR2', function () {
-  gracefulShutdown('nodemon restart', function () {
-    process.kill(process.pid, 'SIGUSR2');
-  }); 
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	}); 
 });
 
 // For app termination
 process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function () {
-    process.exit(0);
-   }); 
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	}); 
 });
 
 // For Heroku app termination
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function () {
-    process.exit(0);
-  });
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
 });
 
 require('./blogs');
+require('./users');
