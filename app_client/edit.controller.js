0eab15c3da66943b1f3dd1f1889c8909355c8a83
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 21:10:54 -0400

    Update edit.controller.js

--- app_client/edit.controller.js
+++ app_client/edit.controller.js
@@ -4,7 +4,7 @@
     .module('bloggerApp')
     .controller('EditController', EditController);
     
-    EditController.$inject['$http', '$routeParams', '$location', 'authentication'];
+    EditController.$inject = ['$http', '$routeParams', '$location', 'authentication'];
     function EditController($http, $routeParams, $location, authentication) {
     var vm = this;
     vm.pageHeader = {
