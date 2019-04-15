fb690daf8e677a70f5fa4be54eb5943a0f7a1036
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:37:45 -0400

    Update list.js

--- app_client/blogs/list.js
+++ app_client/blogs/list.js
@@ -1,27 +1,27 @@
 (function () {
-
-  angular
-    .module('bloggerApp')
-    .controller('ListController', ListController);
+  	angular
+    		.module('bloggerApp')
+    		.controller('ListController', ListController);
     
-    function ListController($http, authentication) {
-    var vm = this;
-    vm.pageHeader = {
-        title: 'Blog List'
-    };
+    	function ListController($http, authentication) {
+    		var vm = this;
+    		vm.pageHeader = {
+        		title: 'Blog List'
+    		};
+		vm.isLoggedIn = function() {
+        		return authentication.isLoggedIn();
+    		}
 	
-	 vm.isLoggedIn = function() {
-        return authentication.isLoggedIn();
-    }
+    		getAllBlogs($http).success(function(data) {
+			vm.blogs = data;
+        		vm.message = "Blog data found!";
+    		}).error(function (e) {
+			vm.message = "Could not get list of blogs";
+    		});
+}
+
+	function getAllBlogs($http) {
+    		return $http.get('/api/blogs');
+	}
 	
-    getAllBlogs($http).success(function(data) {
-		vm.blogs = data;
-        vm.message = "Blog data found!";
-    }).error(function (e) {
-		vm.message = "Could not get list of blogs";
-    });
-}
-function getAllBlogs($http) {
-    return $http.get('/api/blogs');
-}
 })();
