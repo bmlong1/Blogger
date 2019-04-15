f574f454e632f3867e77d036ecd67717287e8e9f
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 20:50:21 -0400

    Create home.controller.js

---
+++ app_client/home.controller.js
@@ -0,0 +1,14 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('HomeController', HomeController);
+    
+    function HomeController() {
+    var vm = this;
+    vm.pageHeader = {
+		title: "My Blogs"
+    };
+	vm.message = "Welcome to my blog site!";
+    }
+    })();
