b1a9fc6e84e3c3e0edb0a8153693a56e413d2c82
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 21:10:05 -0400

    Create login.controller.js

---
+++ app_client/login.controller.js
@@ -0,0 +1,47 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('LoginController', LoginController);
+    
+    LoginController.$inject = ['$http', '$location', 'authentication'];
+
+function LoginController($htttp, $location, authentication) {
+    var vm = this;
+
+    vm.pageHeader = {
+      title: 'Sign in to Blogger'
+    };
+
+    vm.credentials = {
+      email : "",
+      password : ""
+    };
+
+    vm.returnPage = $location.search().page || '/';
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.credentials.email || !vm.credentials.password) {
+           vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+           vm.doLogin();
+      }
+    };
+
+    vm.doLogin = function() {
+      vm.formError = "";
+      authentication
+        .login(vm.credentials)
+        .error(function(err){
+          var obj = err;
+          vm.formError = obj.message;
+        })
+        .then(function(){
+          $location.search('page', null); 
+          $location.path(vm.returnPage);
+        });
+    };
+ }
+ })();
