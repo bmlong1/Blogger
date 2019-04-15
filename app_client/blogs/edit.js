88cf636545d7a7c12888b1159954907d818bd0e4
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:36:01 -0400

    Update edit.js

--- app_client/blogs/edit.js
+++ app_client/blogs/edit.js
@@ -1,39 +1,36 @@
 (function () {
-
-  angular
-    .module('bloggerApp')
-    .controller('EditController', EditController);
+	angular
+    		.module('bloggerApp')
+    		.controller('EditController', EditController);
     
-    EditController.$inject = ['$http', '$routeParams', '$location'];
-function EditController($http, $routeParams, $location, authentication) {
-    var vm = this;
-    vm.pageHeader = {
-       title: 'Blog Edit'
-    };
-    vm.blog = {};       // Start with a blank blog
-    vm.id = $routeParams.id;    // Get id from $routParams which must be injected and passed into controller
+    	EditController.$inject = ['$http', '$routeParams', '$location'];
+	function EditController($http, $routeParams, $location, authentication) {
+    		var vm = this;
+    		vm.pageHeader = {
+       			title: 'Blog Edit'
+    		};
+   		vm.blog = {};      
+    		vm.id = $routeParams.id;  
 	
-    // Get blog data so it may be displayed on edit page
-    getBlogById($http, authentication, vm.id).success(function(data) {
-        vm.blog = data;
-        vm.message = "Blog data found!";
-    }).error(function (e) {
-        vm.message = "Could not get blog given id of " + vm.id;
-    });
     
-    // Submit function attached to ViewModel for use in form
-    vm.submit = function() {
-        var data = vm.blog;
-        data.blogTitle = userForm.blogTitle.value;
-        data.blogText = userForm.blogText.value;
-               
-        updateBlogById($http, authentication, vm.id, data).success(function(data) {
-			vm.message = "Blog data updated!";
-            $location.url('/blog-list');           
-        }).error(function (e) {
-			vm.message = "Could not update blog given id of " + vm.id + userForm.blogTitle.text + " " + userForm.blogText.text;
-        });
-    }
+    		getBlogById($http, authentication, vm.id).success(function(data) {
+       			vm.blog = data;
+        		vm.message = "Blog data found!";
+    		}).error(function (e) {
+        		vm.message = "Could not get blog given id of " + vm.id;
+    		});
+
+    		vm.submit = function() {
+        		var data = vm.blog;
+        		data.blogTitle = userForm.blogTitle.value;
+        		data.blogText = userForm.blogText.value;
+               		updateBlogById($http, authentication, vm.id, data).success(function(data) {
+				vm.message = "Blog data updated!";
+            			$location.url('/blog-list');           
+        		}).error(function (e) {
+				vm.message = "Could not update blog given id of " + vm.id + userForm.blogTitle.text + " " + userForm.blogText.text;
+        		});
+    	}
 }
 
 function getBlogById($http, id) {
