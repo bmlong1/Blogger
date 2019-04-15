9aa61520000224202ce23c775b5fe8408c6a0845
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:13:05 -0400

    Create list.js

---
+++ app_client/blogs/list.js
@@ -0,0 +1,27 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('ListController', ListController);
+    
+    function ListController($http, authentication) {
+    var vm = this;
+    vm.pageHeader = {
+        title: 'Blog List'
+    };
+	
+	 vm.isLoggedIn = function() {
+        return authentication.isLoggedIn();
+    }
+	
+    getAllBlogs($http).success(function(data) {
+		vm.blogs = data;
+        vm.message = "Blog data found!";
+    }).error(function (e) {
+		vm.message = "Could not get list of blogs";
+    });
+}
+function getAllBlogs($http) {
+    return $http.get('/api/blogs');
+}
+})();
