4d0c888919b15adb656e61734b901782c06c05f1
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:31:03 -0400

    Update add.js

--- app_client/blogs/add.js
+++ app_client/blogs/add.js
@@ -1,22 +1,21 @@
 (function () {
+	angular
+    		.module('bloggerApp')
+    		.controller('AddController', AddController);
 
-  angular
-    .module('bloggerApp')
-    .controller('AddController', AddController);
-    
-    AddController.$inject = ['$http', '$location', 'authentication'];
-    
-   function AddController($http, $location, authentication) {
-    var vm = this;
-    vm.blog = {};
-    vm.pageHeader = {
+	AddController.$inject = ['$http', '$location', 'authentication'];
+    	function AddController($http, $location, authentication) {
+    	var vm = this;
+	vm.blog = {};
+    	vm.pageHeader = {
 		title: "Add Blog"
-    };
-    vm.submit = function() {
+    	};
+    
+	vm.submit = function() {
 		var data = vm.blog;
-        data.blogTitle = userForm.blogTitle.value;
-        data.blogText = userForm.blogText.value;
-        addBlog($http, authentication, data).success(function(data) {
+        	data.blogTitle = userForm.blogTitle.value;
+        	data.blogText = userForm.blogText.value;
+        	addBlog($http, authentication, data).success(function(data) {
 			$location.url('/blog-list');
 		}).error(function(e) {
 		});
@@ -26,4 +25,5 @@
 function addBlog($http, authentication, data) {
 	return $http.post('/api/blogs', data, { headers: { Authorization: 'Bearer '+ authentication.getToken() }});
 }
+	
 })();
