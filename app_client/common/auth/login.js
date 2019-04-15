e611f83e80ea24758e48c25850c0a12cf5abadfc
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:41:20 -0400

    Update login.js

--- app_client/common/auth/login.js
+++ app_client/common/auth/login.js
@@ -1,48 +1,43 @@
 (function () {
-
-  angular
-    .module('bloggerApp')
-    .controller('LoginController', LoginController);
+      angular
+            .module('bloggerApp')
+            .controller('LoginController', LoginController);
     
-    
-    LoginController.$inject = ['$http', '$location', 'authentication'];
- function LoginController($htttp, $location, authentication) {
-    var vm = this;
+      LoginController.$inject = ['$http', '$location', 'authentication'];
+            function LoginController($htttp, $location, authentication) {
+            var vm = this;
+                  vm.pageHeader = {
+              title: 'Sign in to Blogger'
+            };
+            vm.credentials = {
+                  email : "",
+                  password : ""
+            };
+            vm.returnPage = $location.search().page || '/';
 
-    vm.pageHeader = {
-      title: 'Sign in to Blogger'
-    };
+            vm.onSubmit = function () {
+                  vm.formError = "";
+                  if (!vm.credentials.email || !vm.credentials.password) {
+                        vm.formError = "All fields required, please try again";
+                        return false;
+                  } else {
+                        vm.doLogin();
+                  }
+            };
 
-    vm.credentials = {
-      email : "",
-      password : ""
-    };
-
-    vm.returnPage = $location.search().page || '/';
-
-    vm.onSubmit = function () {
-      vm.formError = "";
-      if (!vm.credentials.email || !vm.credentials.password) {
-           vm.formError = "All fields required, please try again";
-        return false;
-      } else {
-           vm.doLogin();
-      }
-    };
-
-    vm.doLogin = function() {
-      vm.formError = "";
-      authentication
-        .login(vm.credentials)
-        .error(function(err){
-          var obj = err;
-          vm.formError = obj.message;
-        })
-        .then(function(){
-          $location.search('page', null); 
-          $location.path(vm.returnPage);
-        });
-    };
+            vm.doLogin = function() {
+                  vm.formError = "";
+                  authentication
+                        .login(vm.credentials)
+                        .error(function(err){
+                              var obj = err;
+                              vm.formError = obj.message;
+                         })
+                         .then(function(){
+                              $location.search('page', null); 
+                              $location.path(vm.returnPage);
+                        });
+            };
  }
  
  })();
