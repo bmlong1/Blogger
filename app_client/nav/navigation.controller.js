e8b100215d29cd272138bb35475f37161deb0964
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 21:30:23 -0400

    Create navigation.controller.js

---
+++ app_client/nav/navigation.controller.js
@@ -0,0 +1,22 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('NavigationController', NavigationController);
+
+ NavigationController.$inject = ['$location', 'authentication'];
+  function navigationCtrl($location, authentication) {
+var vm = this;
+    vm.currentPath = $location.path();
+    vm.currentUser = function()  {
+        return authentication.currentUser();
+    }
+    vm.isLoggedIn = function() {
+        return authentication.isLoggedIn();
+    }
+    vm.logout = function() {
+        authentication.logout();
+        $location.path('/').replace();
+    };
+  }
+})();
