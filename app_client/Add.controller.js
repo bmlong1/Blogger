ee774a5ca5cf90d75354b02b6f7fdb4625079da9
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-14 20:55:39 -0400

    Create Add.controller.js

---
+++ app_client/Add.controller.js
@@ -0,0 +1,28 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('AddController', AddController);
+
+AddController.$inject['$http', '$location', authentication];
+  function AddController($http, $location, authentication) {
+   var vm = this;
+    vm.blog = {};
+    vm.pageHeader = {
+		title: "Add Blog"
+    };
+    vm.submit = function() {
+		var data = vm.blog;
+        data.blogTitle = userForm.blogTitle.value;
+        data.blogText = userForm.blogText.value;
+        addBlog($http, authentication, data).success(function(data) {
+			$location.url('/blog-list');
+		}).error(function(e) {
+		});
+	};
+  }
+
+function addBlog($http, authentication, data) {
+	return $http.post('/api/blogs', data, { headers: { Authorization: 'Bearer '+ authentication.getToken() }});
+}
+})();
