10c519304413cdfc406da15a0a0cc3c4ea315ac3
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 07:50:24 -0400

    Update home.js

--- app_client/home/home.js
+++ app_client/home/home.js
@@ -1,1 +1,14 @@
-stuff
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('HomeController', HomeController);
+
+ function HomeController() {
+	var vm = this;
+    vm.pageHeader = {
+		title: "My Blogs"
+    };
+	vm.message = "Welcome to my blog site!";
+}
+})();
