37be614a32fb90e61ce2b341448200dec718b484
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-15 10:18:20 -0400

    Create delete.js

---
+++ app_client/blogs/delete.js
@@ -0,0 +1,44 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('DeleteController', DeleteController);
+    
+    DeleteController.$inject = ['$http', '$routeParams', '$location', 'authentication'];
+    
+function DeleteController($http, $routeParams, $location, authentication) {
+	var vm = this;
+	vm.pageHeader = {
+		title: 'Delete Blog'
+	};
+
+	vm.blog = {};
+	vm.id = $routeParams.id;
+
+	getBlogById($http, authentication, vm.id).success(function(data) {
+		vm.blog = data;
+		vm.message = "Blog data found!";
+	}).error(function(e) {
+		vm.message = "Could not get blog given id";
+	});
+
+	vm.submit = function() {
+		var data = vm.blog;
+		deleteBlogById($http, authentication, vm.id, data).success(function(data) {
+			vm.message = "Blog deleted";
+			$location.url('/blog-list');
+		}).error(function(e) {
+			vm.message = "Could not delete blog given id";
+		});
+	}
+}
+function getBlogById($http, id) {
+    return $http.get('/api/blogs/' + id);
+}
+
+
+function deleteBlogById($http, authentication, id, data) {
+    return $http.delete('/api/blogs/' + id, data, { headers: { Authorization: 'Bearer '+ authentication.getToken() }});
+}
+
+})();
