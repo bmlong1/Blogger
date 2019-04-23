afb77917c89b4b101654959bbc98a491fcf3c4dc
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-22 21:56:12 -0400

    Update blogs.js

--- app_api/models/blogs.js
+++ app_api/models/blogs.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var blogSchema = new mongoose.Schema({
     blogTitle: String,
     blogText: String,
+    author: String,
     createdOn: {
         type: Date,
         'default': Date.now
