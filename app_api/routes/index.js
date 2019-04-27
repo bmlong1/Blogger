c112cbb9428382c15a8e1685fdb7df2a779ff2ba
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-27 10:18:29 -0400

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -15,5 +15,6 @@ router.put('/blogs/:blogid', auth, ctrlBlog.blogUpdateOne);
 router.delete('/blogs/:blogid', auth, ctrlBlog.blogDeleteOne);
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
-router.get('/login', ctrlAuth.onlineUserList);
+router.post('/user', ctrlAuth.onlineUserAss);
+router.get('/user', ctrlAuth.onlineUserList);
 module.exports = router;
