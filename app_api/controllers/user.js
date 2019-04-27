7b01b887e3b1cf8fc59fb94b431b40bb72b1fa85
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-04-27 10:22:47 -0400

    Create user.js

---
+++ app_api/controllers/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const OnlineUser = mongoose.model('OnlineUsers');
+
+var sendJSONResponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+const onlineUserList = function (req, res) {
+    OnlineUser.find().exec(function(err, results) {
+        if (!results) {
+           sendJSONresponse(res, 404, {
+               "message" : "No users found"
+           });
+        } else if (err) {
+            sendJSONresponse(res, 404, err);
+            return;
+        }
+        sendJSONresponse(res, 200, buildOnlineUserList(req, res, results));
+    });
+};        
+
+const buildOnlineUserList = function(req, res, results) {
+    var onlineUsers = [];
+    results.forEach(function(obj) {
+        onlineUsers.push({
+           userName: obj.userName,
+	   userEmail: obj.userEmail
+        });
+    });
+    return onlineUsers;
+};
+
+const onlineUserAdd = function (user) {
+    OnlineUser.create({
+        userName: user.name,
+        userEmail: user.email,
+    }, function(err, onlineuser) {
+      if(err) {
+        send JSONResponse(res, 400, err);
+      } else {
+      send JSONResponse(res, 201, onlineuser);
+      }
+    });
+};
+
+module.exports = {
+    onlineUserList,
+    onlineUserAdd
+};
