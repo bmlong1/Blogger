21202b4f04db4f6067c13025d784e48d93f33324
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-02 16:40:22 -0400

    Update authentication.js

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -88,6 +88,6 @@ var buildUserList = function(req, res, results) {
 
 module.exports.userDelete = function(req, res) {
 	console.log(req.params.email);
-	OnlineUser.deleteMany({email: req.params.email}, function(err) {});
+	OnlineUser.deleteMany({email: "brianamarielong@aol.com"}, function(err) {});
 };
 
