35384c44a48ec087950df744fc04d51a5824a3ed
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-03 14:42:23 -0400

    Update authentication.service.js

--- app_client/auth/authentication.service.js
+++ app_client/auth/authentication.service.js
@@ -45,7 +45,7 @@
         var logout = function() {
           var thisEmail = currentUser().email;
             $window.localStorage.removeItem('blog-token');
-          return $http.delete('/api/allUsers/' + thisEmail);
+          return $http.delete('/api/user/' + thisEmail);
         };
         
         var isLoggedIn = function() {
