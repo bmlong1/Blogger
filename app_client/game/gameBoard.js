e3dd7a9e75e955768ad0e56d3a5136d78b839bcb
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 15:00:47 -0400

    Update gameBoard.js

--- app_client/game/gameBoard.js
+++ app_client/game/gameBoard.js
@@ -8,25 +8,15 @@ function GameBoardController($http, $scope, $interval, $location, authentication
 	var vm = this;
 	
 	vm.otherPlayer = function() {
-		$http.get('/api/gameUsers').success(function(data) {
-			data.forEach(function(element) {
-				if (element.challenger == vm.currentUser()) {
-					return (element.player);
-				} else if (element.player == vm.currentUser()){
-					return element.challenger;
-				}
-			});
-		});
+		return authentication.otherPlayer();
 	};
 	
     vm.pageHeader = {
-		title: "Battle Ship",
-	    	heading1: "You ",
-	    	heading2: " to play a game of Battle Ship."
+		title: "Battle Ship"
     };
 	
 	vm.currentUser = function() {
-		return authentication.currentUser();
+		return authentication.currentUser().name;
 	};
 }
 })();
