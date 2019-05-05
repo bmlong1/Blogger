f150812ab087f82d1a1283543a80b4933443c5a5
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 14:38:45 -0400

    Update gameBoard.js

--- app_client/game/gameBoard.js
+++ app_client/game/gameBoard.js
@@ -7,8 +7,34 @@ GameBoardController.$inject = ['$http', '$scope', '$interval', '$location', 'aut
 function GameBoardController($http, $scope, $interval, $location, authentication) {
 	var vm = this;
     vm.pageHeader = {
-		title: "Game Board",
-	    	heading: "Game Board"
+		title: "Battle Ship",
+	    	heading: "You " + vm.challengeOrPlayer() + vm.otherPlater() " to play a game of Battle Ship."
     };
+	vm.challengerOrPlayer = function() {
+		$http.get('/api/gamUsers').success(function(data) {
+			data.forEach(function(element) {
+				if(element.challenger == vm.currentUser()) {
+					return "challenged";
+				} else {
+					return "were challenged by ";
+				}
+			});
+		});
+	};
+	vm.otherPlayer = function() {
+		$http.get('/api/gameUsers').success(function(data) {
+			data.forEach(function(element) {
+				if (element.challenger == vm.currentUser()) {
+					return element.player;
+				} else if (element.player == vm.currentUser()){
+					return element.challenger;
+				}
+			});
+		});
+	};
+	
+	vm.currentUser = function() {
+		return authentication.currentUser();
+	};
 }
 })();
