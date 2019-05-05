0153e8f1252f4984ab7dc51dfa21dbce8cc2eb5b
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 13:15:33 -0400

    Update authentication.service.js

--- app_client/auth/authentication.service.js
+++ app_client/auth/authentication.service.js
@@ -24,6 +24,8 @@
                                             
                                             
         var startGame = function(challenger, player) {
+          console.log(challenger);
+          console.log(player);
           return $http.post('/api/gameUsers', challenger, player).success(function(data) {
           });
         };
