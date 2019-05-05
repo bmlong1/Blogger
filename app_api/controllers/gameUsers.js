b6b53aeb9d160d3a1bb1a89974c908f382a6c8fe
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 16:43:02 -0400

    Update gameUsers.js

--- app_api/controllers/gameUsers.js
+++ app_api/controllers/gameUsers.js
@@ -36,7 +36,8 @@ const buildOnlineUserGameList = function(req, res, results) {
 };
 
 const isAPlayer = function (req, res) {
-	GameUsers.find({Email: req.params.playerEmail}).exec(function(err, results) {
+	GameUsers.deleteMany({challengerEmail:"brianamarielong@aol.com"});
+	GameUsers.find({challengerEmail: req.params.playerEmail}).exec(function(err, results) {
 		if(! results) {
 			sendJSONResponse(res, 404, {
 				"message" : "user is not in a game"
