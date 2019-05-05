f0972ccdc19bdd8ec6b2583cb78d68361e62fca4
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 14:58:21 -0400

    Update gameBoard.js

--- app_client/game/gameBoard.js
+++ app_client/game/gameBoard.js
@@ -6,19 +6,6 @@
 GameBoardController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
 function GameBoardController($http, $scope, $interval, $location, authentication) {
 	var vm = this;
-		vm.challengerOrPlayer = function() {
-			$http.get('/api/gameUsers').success(function(data) {
-			data.forEach(function(element) {
-				if(element.challenger == vm.currentUser()) {
-					console.log("challenge");
-					return "challenged ";
-				} else {
-					console.log("player");
-					return "were challenged by ";
-				}
-			});
-		});
-		};
 	
 	vm.otherPlayer = function() {
 		$http.get('/api/gameUsers').success(function(data) {
