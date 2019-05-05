7a82629581bee6ac70ce068ed5c753fe7a39a3d9
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 13:33:50 -0400

    Update gameBoard.js

--- app_client/game/gameBoard.js
+++ app_client/game/gameBoard.js
@@ -3,8 +3,8 @@
   angular
     .module('bloggerApp')
     .controller('GameBoardController', GameBoardController);
-GameController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
-function GameController($http, $scope, $interval, $location, authentication) {
+GameBoardController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
+function GameBoardController($http, $scope, $interval, $location, authentication) {
 	var vm = this;
     vm.pageHeader = {
 		title: "Game Board",
