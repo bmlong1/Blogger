ffd73414d52e9776f5ea96b6dfac155f15bdbacf
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 12:36:40 -0400

    Update game.js

--- app_client/game/game.js
+++ app_client/game/game.js
@@ -46,7 +46,7 @@ function GameController($http, $scope, $interval, $location, authentication) {
 	vm.submit = function() {
 for(var i = 0; i < userForm.length; i++) {
 	if(userForm[i].userrr.checked == "checked") {
-		console.log(userForm[i].userrr);
+		console.log(userForm[i].userrr.value);
 	}
 }
 		
