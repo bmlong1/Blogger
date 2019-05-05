a638bed4f51423b4acc20f5b6794acc64b89af92
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-04 22:45:53 -0400

    Update onlineUser.js

--- app_api/controllers/onlineUser.js
+++ app_api/controllers/onlineUser.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const OnlineUser = mongoose.model('OnlineUsers');
+const GameUsers = mongoose.model('GameUsers');
 
 var sendJSONResponse = function(res, status, content) {
     res.status(status);
@@ -31,6 +32,40 @@ const buildOnlineUserList = function(req, res, results) {
     return onlineUsers;
 };
 
+
+
+
+
+const onlineUserGameList = function (req, res) {
+    GameUsers.find().exec(function(err, results) {
+        if (!results) {
+           sendJSONResponse(res, 404, {
+               "message" : "No users found"
+           });
+        } else if (err) {
+            sendJSONResponse(res, 404, err);
+            return;
+        }
+        sendJSONResponse(res, 200, buildOnlineUserGameList(req, res, results));
+    });
+};        
+
+const buildOnlineUserGameList = function(req, res, results) {
+    var onlineUsers = [];
+    results.forEach(function(obj) {
+        onlineUsers.push({
+           challenger: obj.challenger,
+	   player: obj.player
+        });
+    });
+    return onlineUsers;
+};
+
+
+
+
+
+
 const onlineUserAdd = function (req, res) {
 	if(OnlineUser.find({userEmail: req.params.email}).userEmail != undefined) {
 	   }else {
@@ -47,6 +82,23 @@ const onlineUserAdd = function (req, res) {
 }
 };
 
+const onlineUserStartGame = function(req, rea) {
+	GameUsers.create){
+		challenger: req.body.challenger,
+		player: req.body.player
+	}, function(err, gameUsers) {
+		if(err) {
+			sendJSONResponse(res, 400, err);
+		}else {
+			sendJSONResponse(res, 201, gameUsers);
+		}
+	});
+};
+
+const onlineUserDeleteGame = function(req, res) {
+	OnlineUser.deleteMany({challenger: req.params.challenger, player: req.params.player}, function(err) {});
+};
+
 const onlineUserDelete = function(req, res) {
 	OnlineUser.deleteMany({userEmail: req.params.email}, function(err) {});
 };
@@ -54,5 +106,8 @@ const onlineUserDelete = function(req, res) {
 module.exports = {
     onlineUserList,
     onlineUserAdd,
-    onlineUserDelete
+    onlineUserDelete,
+	onlineUserStartGame,
+	onlineUserDeleteGame,
+	onlineUserGameList
 };
