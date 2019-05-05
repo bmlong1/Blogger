0831ece87359003a3398950b9da9f0920e46abd3
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 17:27:16 -0400

    Update gameBoard.js

--- app_client/game/gameBoard.js
+++ app_client/game/gameBoard.js
@@ -9,6 +9,7 @@ function GameBoardController($http, $scope, $interval, $location, authentication
 	vm.challenger = function() {
 		getGamers($http).success(function(data) {
 			data.forEach(function(element) {
+				console.log(element.challengerEmail);
 				if(element.challengerEmail == vm.currentUser().email) {
 					return vm.currentUser().name;
 				} else if (element.playerEmail == vm.currentUser().email){
