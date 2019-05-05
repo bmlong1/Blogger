6195bba44e4e34279f123862da3043e19f395450
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-05 18:15:39 -0400

    Update game.js

--- app_client/game/game.js
+++ app_client/game/game.js
@@ -48,7 +48,7 @@ function GameController($http, $scope, $interval, $location, authentication) {
 	
 	vm.submit = function() {
 		vm.userName = userForm.userName.value;
-		console.log(vm.userForm);
+		console.log(userForm);
 		vm.userEmail = userForm.userEmail.value;
 		console.log(vm.userEmail);
 		/*$http.post('/api/game/' + vm.currentUser().name + "/" + vm.currentUser().email + "/" +
