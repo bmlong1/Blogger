cff24a55bdf670de6542b01b60b3edcf786764d3
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 18:21:17 -0400

    Update game.js

--- app_api/controllers/game.js
+++ app_api/controllers/game.js
@@ -92,7 +92,7 @@ const buildOnlineUserGameList = function(req, res, results) {
 const isAPlayer = function (req, res) {
 	/*GameUsers.deleteMany({challengerEmail:"brianamarielong@aol.com"}, function(err){});
 	GameUsers.deleteMany({challengerEmail:"bmlong1@millersville.edu"}, function(err){});*/
-	GameUsers.find({challengerEmail: req.params.playerEmail}).exec(function(err, results) {
+	GameUsers.find({playerEmail: req.params.playerEmail}).exec(function(err, results) {
 		if(! results) {
 			GameUsers.find({playerEmail: req.params.playerEmail}).exec(function(err, resultss) {
 		if(! resultss) {
