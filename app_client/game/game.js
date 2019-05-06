d2ae57fb9901127563d0d1567a5a042e5f6f8357
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 10:16:22 -0400

    Update game.js

--- app_client/game/game.js
+++ app_client/game/game.js
@@ -49,24 +49,6 @@ function GameController($http, $scope, $interval, $location, authentication) {
 		}
 		$interval( function(){$scope.callAtInterval();}, 3000, 0, true);
 	
-	vm.submit = function() {
-		vm.userName = userForm.userName.value;
-		console.log(vm.userName);
-		vm.userEmail = userForm.userEmail.value;
-		console.log(vm.userEmail);
-		$http.post('/api/game/' + vm.currentUser().name + "/" + vm.currentUser().email + "/" +
-			  vm.userName + "/" + vm.userEmail).success(function(){
-			$location.url('/game-board');
-			});
-    	};
 
 }
-function getOnlineUsers($http) {
-	return $http.get('/api/user');
-};
-
-	function changeHeading() {
-		vm.pageHeader.heading = "you are playing as";
-	};
-
 })();
