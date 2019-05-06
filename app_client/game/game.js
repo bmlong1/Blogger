677cc352f4e3e7c90672507334533e34c3b8e4c5
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 17:51:12 -0400

    Update game.js

--- app_client/game/game.js
+++ app_client/game/game.js
@@ -1,65 +1,3 @@
-/*(function () {
-
-  angular
-    .module('bloggerApp')
-    .controller('GameController', GameController);
-GameController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
-function GameController($http, $scope, $interval, $location, authentication) {
-	var vm = this;
-	
-	vm.currentUser = function() {
-		return authentication.currentUser();
-	};
-
-	
-	vm.yourPlayer = function() {
-		if(vm.x.email == vm.currentUser().email) {
-			return "X's";
-		} else if (vm.o.email == vm.currentUser().email) {
-			return "O's";
-		}
-	};
-		
-	vm.pageHeader = {
-		title: "Play a Game",
-	    	heading: "Would you like to be X's or O's?",
-		other: "You are playing as "
-    	};
-	
-	
-	vm.noPlayer = function() {
-		if (vm.x == undefined && vm.o == undefined) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-	
-	
-	vm.playerX = function() {
-		vm.x = vm.currentUser();
-		vm.hideX = true;
-	};
-	
-	vm.playerO = function() {
-		vm.o = vm.currentUser();
-		vm.hideO = true;
-	};
-	
-	
-	// Refreshes lists of users periodically					  
-		$scope.callAtInterval = function() {
-//			vm.yourPlayer();	
-		}
-		$interval( function(){$scope.callAtInterval();}, 3000, 0, true);
-	
-
-}
-})();*/
-
-
-
-
 (function () {
 
   angular
@@ -78,6 +16,7 @@ function GameController($http, $scope, $interval, $location, authentication) {
 		return authentication.currentUser();
 	};
 	$http.get('/api/game/'+vm.currentUser().email).success(function(data) {
+		console.log("hey");
 		if(data.length > 0) {
 				$location.url('/game-board');	
 
