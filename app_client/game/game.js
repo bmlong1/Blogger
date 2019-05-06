d15c5490ca1b79f57a44a1c539e33dac3c90b5fd
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 17:26:56 -0400

    Update game.js

--- app_client/game/game.js
+++ app_client/game/game.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
 
   angular
     .module('bloggerApp')
@@ -55,4 +55,75 @@ function GameController($http, $scope, $interval, $location, authentication) {
 	
 
 }
+})();*/
+
+
+
+
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('GameController', GameController);
+GameController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
+function GameController($http, $scope, $interval, $location, authentication) {
+	var vm = this;
+		
+	vm.pageHeader = {
+		title: "Play a Game",
+	    	heading: "Who would you like to play a game with?"
+    	};
+	
+	vm.currentUser = function() {
+		return authentication.currentUser();
+	};
+	$http.get('/api/game/'+vm.currentUser().email).success(function(data) {
+		if(data.length > 0) {
+				$location.url('/game-board');	
+
+		}
+		
+	});
+	
+	
+	 getOnlineUsers($http).success(function(data) {
+			vm.users = data;
+        		vm.message = "User data found!";
+			console.log(vm.users);
+    		}).error(function (e) {
+			vm.message = "Could not get list of Users";
+    		});
+	
+	
+	// Refreshes lists of users periodically					  
+		$scope.callAtInterval = function() {
+			getOnlineUsers($http)
+			  .success(function(data) {
+				vm.users = data;
+				vm.message = "Users list found!";
+			  })
+			  .error(function (e) {
+				vm.message = "Could not get list of users";
+			});	
+		}
+		$interval( function(){$scope.callAtInterval();}, 3000, 0, true);
+	
+	vm.submit = function() {
+		vm.userName = userForm.userName.value;
+		console.log(vm.userName);
+		vm.userEmail = userForm.userEmail.value;
+		console.log(vm.userEmail);
+		$http.post('/api/game/' + vm.currentUser().name + "/" + vm.currentUser().email + "/" +
+			  vm.userName + "/" + vm.userEmail).success(function(){
+			$location.url('/game-board');
+			});
+    	};
+
+}
+function getOnlineUsers($http) {
+	return $http.get('/api/user');
+};
+
+
 })();
+
