888f8712aad2eee25c729098e540117d68163b90
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 20:15:33 -0400

    Create chat.js

---
+++ app_client/chat/chat.js
@@ -0,0 +1,37 @@
+(function () {
+
+  angular
+    .module('bloggerApp')
+    .controller('ChatController', ChatController);
+ChatController.$inject = ['$http', '$scope', '$interval', '$location', 'authentication'];
+function GameController($http, $scope, $interval, $location, authentication) {
+	var vm = this;
+		
+	vm.pageHeader = {
+		title: "Chat with Friends",
+	    	heading: "You can chat with the other users who are online, or you can just browse what has been said previously."
+    	};
+	
+	vm.currentUser = function() {
+		return authentication.currentUser();
+	};
+	
+	
+	
+	// Refreshes lists of users periodically					  
+		$scope.callAtInterval = function() {
+			getComments($http)
+			  .success(function(data) {
+				vm.comments = data;
+			  })
+			  .error(function (e) {
+			});
+			vm.redirect();
+		}
+		$interval( function(){$scope.callAtInterval();}, 3000, 0, true);
+	
+}
+function getComments($http) {
+  return $http.get('/chat');
+};
+})();
