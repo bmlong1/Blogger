9355b8fde3d2a18fb98d0d86fadcc8f3d0ba6db0
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2019-05-06 20:04:34 -0400

    Delete onlineUsers.js

--- app_api/models/onlineUsers.js
+++
@@ -1,9 +0,0 @@
-    
-var mongoose = require('mongoose');
-
-var onlineUserSchema = new mongoose.Schema({
-    userName: String,
-    userEmail: String
-});
-
-mongoose.model('OnlineUsers', onlineUserSchema);
